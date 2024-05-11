c5be5e3b38209076ba1e065a9ab36f26fff7d646
yuqi <fuyuqi1995@gmail.com>
2024-05-11 18:33:23 -0400

    add backend

---
+++ backend/sched_common.h
@@ -0,0 +1,9 @@
+typedef struct func_sched_item{
+        unsigned long p1;
+        unsigned long p2;
+}func_sched_item;
+
+typedef struct func_sched_execution{
+        unsigned long total;
+        unsigned long amount;
+}func_sched_execution;
